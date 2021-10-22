ff249f92b6d885917f3b6e604de4f814f5b1ebc7
jams <jamesqiyang@gmail.com>
2021-10-22 14:32:40 -0400

    added try catches to prevent crashes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client, Intents, MessageEmbed, MessageAttachment} = require("discord.js");
+const { Client, Intents, MessageEmbed, MessageAttachment } = require("discord.js");
 const tmi = require('tmi.js');
 
 const fetch = require("node-fetch");
@@ -16,10 +16,10 @@ const command_symbol = '!';
 const bot_intents = new Intents();
 bot_intents.add(
   Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES, 
+  Intents.FLAGS.GUILD_MESSAGES,
   Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
   Intents.FLAGS.DIRECT_MESSAGES);
-const discord_client = new Client({intents:bot_intents});
+const discord_client = new Client({ intents: bot_intents });
 
 // TWITCH BOT
 const opts = {
@@ -34,9 +34,9 @@ const opts = {
 const twitch_client = new tmi.client(opts);
 
 // functions 
-function getQuote(){
-  return fetch ("https://zenquotes.io/api/random")
-    .then (res => {
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
       return res.json();
     })
     .then(data => {
@@ -52,76 +52,88 @@ twitch_client.on('message', onMessageTwitch);
 twitch_client.on('connected', onConnectedTwitch);
 
 
-function onConnectedDiscord(){
+function onConnectedDiscord() {
   console.log("DISCORD bot ready: " + discord_client.user.tag);
 }
 
 // bot sends message depending on command given
 // input: discord message object
 // output: none
-function onMessageDiscord(message){
-  if (message.author.bot) return false; 
-  
-  // check if the first word is a command
-  if (message.content.substring(0,command_symbol.length) == command_symbol){
-    let argv = message.content.trim().split(' ');
+function onMessageDiscord(message) {
+  if (message.author.bot) return false;
+  try {
+    // check if the first word is a command
+    if (message.content.substring(0, command_symbol.length) == command_symbol) {
+      let argv = message.content.trim().split(' ');
+      var command = argv[0].substring(command_symbol.length);
+      switch (command) {
+        case "calc":
+          argv.shift()
+          let result = processCalcInput.getEmbed(argv);
+          sendMessageDiscord(message, result);
+          break;
+        case "quote":
+          "Quote: " + getQuote().then(quote => message.channel.send(quote));
+          break;
+        case "help":
+          sendMessageDiscord(message, help.getHelpEmbed());
+          break;
+        case "manCalc":
+          sendMessageDiscord(message, help.getManEmbed());
+          break;
+        default:
+          message.channel.send("invalid command, try again.");
+      }
+      console.log(`* Discord: Executed ${command} command`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function sendMessageDiscord(message, payload) {
+  try {
+    message.channel.send(payload);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function onMessageTwitch(target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+  try {
+    // Remove whitespace from chat message
+    let argv = msg.trim().split(' ');
     var command = argv[0].substring(command_symbol.length);
-    switch(command){
+    switch (command) {
       case "calc":
         argv.shift()
-        let result = processCalcInput.getEmbed(argv);
-        message.channel.send(result);
+        let result = processCalcInput.getString(argv);
+        twitch_client.say(target, result);
         break;
-      case "quote":
-        "Quote: " + getQuote().then(quote => message.channel.send(quote));
+      case "dice":
+        const num = rollDice();
+        twitch_client.say(target, `You rolled a ${num}`);
         break;
       case "help":
-        message.channel.send(help.getHelpEmbed());
+        twitch_client.say(target, help.genCalc());
         break;
       case "manCalc":
-        message.channel.send(help.getManEmbed());
+        twitch_client.say(target, help.genManCalc());
         break;
       default:
-        message.channel.send("invalid command, try again.");
+        twitch_client.say(target, "invalid command, try again.");
     }
-    console.log(`* Discord: Executed ${command} command`);
+  } catch (error) {
+    console.log(error);
   }
 }
 
-
-
-function onMessageTwitch (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  let argv = msg.trim().split(' ');
-  var command = argv[0].substring(command_symbol.length);
-  switch(command){
-    case "calc":
-      argv.shift()
-      let result = processCalcInput.getString(argv);
-      twitch_client.say(target, result);
-      break;
-    case "dice":
-      const num = rollDice();
-      twitch_client.say(target, `You rolled a ${num}`);
-      break;
-    case "help":
-      twitch_client.say(target, help.genCalc());
-      break;
-    case "manCalc":
-      twitch_client.say(target, help.genManCalc());
-      break;
-    default:
-      twitch_client.say(target, "invalid command, try again.");
-  }
-}
-
-function onConnectedTwitch (addr, port) {
+function onConnectedTwitch(addr, port) {
   console.log(`* TWITCH BOT ready. Connected to ${addr}:${port}`);
 }
 
-function rollDice () {
+function rollDice() {
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
 }
